e48da6ca9140f8b2e01a6d0388662c68755219c9
gforget <gforget2@gmail.com>
2025-01-24 22:34:23 -0500

    first commit

---
+++ EQSTesting/Source/EQSTesting/EQSTesting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EQSTesting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EQSTesting, "EQSTesting" );
